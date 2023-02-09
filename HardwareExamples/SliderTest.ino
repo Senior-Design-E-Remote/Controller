17150d2f385057c4d409663e0a778207c24265d2
Nick Gutierrez <60188339+ng0805@users.noreply.github.com>
2023-02-09 17:50:19 -0500

    Slider for Color Shift LEDs

--- HardwareExamples/SliderTest.ino
+++ HardwareExamples/SliderTest.ino
@@ -1,18 +1,29 @@
 #define slide A0
 #define led 6
 
+int oldslide;
+
 void setup() {
-  // put your setup code here, to run once:
+  //put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(slide,INPUT);
   pinMode(led, OUTPUT);
+  oldslide = analogRead(slide);
+  analogWrite(led,128);
 }
 
+
+
 void loop() {
-  // put your main code here, to run repeatedly:
+  //put your main code here, to run repeatedly:
   int slideVal = analogRead(slide);
-  int ledVal = map(slideVal, 0 , 1024, 0, 255);
-  Serial.print("Slide val: ");
-  Serial.println(slideVal);
-  analogWrite(led, ledVal);
+  int ledVal = map(slideVal, 0 , 1024, 0, 128);
+  if(slideVal > (oldslide+5) || slideVal < (oldslide-5)){
+    analogWrite(led, ledVal);
+    oldslide = slideVal;
+    Serial.print("Old Slide Changed to ");
+    Serial.println(oldslide);
+  }
+  // digitlaWrite(led,1);
+  // analogWrite(led,128);
 }
