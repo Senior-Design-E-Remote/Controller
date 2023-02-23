f105d0d0d0d1f9bd39f8da5f854af052ec8c6f4a
Nick Gutierrez <60188339+ng0805@users.noreply.github.com>
2023-02-23 16:32:17 -0500

    Removed Battery Level Report

--- WirelessController.ino
+++ WirelessController.ino
@@ -29,9 +29,7 @@ const byte buttonNames[numButtons] = {BUTTON_1, BUTTON_2, BUTTON_3, BUTTON_4, BU
 bool oldButtonStates[numButtons] = {0,0,0,0,0,0,0,0,0,0};//,0,0,0,0};
 
 uint16_t coreCurrtouched = 0;
-// uint16_t dpCurrtouched = 0;
 Adafruit_MPR121 coreCap = Adafruit_MPR121();
-// Adafruit_MPR121 dpCap = Adafruit_MPR121();
 
 BleGamepad bleGamepad("E-Remote", "Team 24", 100);
 
@@ -48,13 +46,6 @@ void setup()
 
   Serial.println("Core Cap Connected");
 
-  // while(!dpCap.begin(0x5B)){
-
-  //   Serial.println("DPad Cap Not Connected");
-  // }
-
-  // Serial.println("DPad Cap Connected");
-
   for(int i = 0; i < 2; i++){ // Initialize all buttons
 
     pinMode(buttons[i], INPUT_PULLUP);
@@ -64,99 +55,66 @@ void setup()
   bleGamepadConfig.setWhichAxes(true, true, true, false, false, true, false, false); //only enable X, Y, Z, RZ
   bleGamepadConfig.setWhichSimulationControls(false,false,false,false,false);
   bleGamepad.begin(&bleGamepadConfig);
-  //The default bleGamepad.begin() above enables 16 buttons, all axes, one hat, and no simulation controls or special buttons
 }
 
 void loop()
 {   
-    if (bleGamepad.isConnected())
-    {
-      if(batteryCount >= 30){ //average battery level over time before reporting
-        
-      batteryLevel = (batteryLevel + analogRead(batteryPin))/batteryCount; //take one more reading and average the fotal readings
-
-      float voltage = (batteryLevel * (5.0 / 1023.0))*2;
-
-      uint8_t bl = ((uint8_t) voltage*100) - 320; //cast float to unit6_t and convert to battery percentage (LiPoly ranges from 3.2 to 4.2V)
-
-      if(bl != oldBatteryLevel){
-
-        bleGamepad.setBatteryLevel(bl);
-        oldBatteryLevel = bl; 
+  if (bleGamepad.isConnected())
+  {
+    bool buttonState;
+    coreCurrtouched = coreCap.touched();
+    // dpCurrtouched = dpCap.touched();
+    
+    for(int i = 0; i < numButtons; i++){ //Set button values, subtract 4 for the capacitive touch buttons
+      if(i < 2){
+        buttonState = digitalRead(buttons[i]); //Normal Buttons    
+      }
+      else if(i >=2 && i < 11){ //core 8 buttons
+        buttonState = coreCurrtouched & _BV(buttons[i]); //Core 8 Capacitive Buttons
       }
 
-        batteryCount = 0; // reset count and battery level
-
-        batteryLevel = 0;
-      }
-      else{
-        batteryLevel = batteryLevel + analogRead(batteryPin);
-
-        batteryCount = batteryCount+1;
-      }
-
-      bool buttonState;
-      coreCurrtouched = coreCap.touched();
-      // dpCurrtouched = dpCap.touched();
-      
-      for(int i = 0; i < numButtons; i++){ //Set button values, subtract 4 for the capacitive touch buttons
-        if(i < 2){
-          buttonState = digitalRead(buttons[i]); //Normal Buttons    
-        }
-        else if(i >=2 && i < 11){ //core 8 buttons
-          buttonState = coreCurrtouched & _BV(buttons[i]); //Core 8 Capacitive Buttons
-        }
-        // else{
-        //   buttonState = dpCurrtouched & _BV(buttons[i]); //DPad Capacitive Buttons
-        // }
-
-        if(oldButtonStates[i] != buttonState){ //Only send new signal if button state changed
-          oldButtonStates[i] = buttonState;
-          if(buttonState){
-            bleGamepad.press(buttonNames[i]);
-            Serial.print("Button ");
-            Serial.print(i);
-            Serial.println(" Pressed");
-          }          
-          else{
-            bleGamepad.release(buttonNames[i]);
-            Serial.print("Button ");
-            Serial.print(i);
-            Serial.println(" Unpressed");           
-          }
+      if(oldButtonStates[i] != buttonState){ //Only send new signal if button state changed
+        oldButtonStates[i] = buttonState;
+        if(buttonState){
+          bleGamepad.press(buttonNames[i]);
+          Serial.print("Button ");
+          Serial.print(i);
+          Serial.println(" Pressed");
+        }          
+        else{
+          bleGamepad.release(buttonNames[i]);
+          Serial.print("Button ");
+          Serial.print(i);
+          Serial.println(" Unpressed");           
         }
       }
-
-      //Set Joystick Values. Note: Joysticks need 3.3V?
-
-      int leftX = 0;
-      int leftY = 0;
-      int rightX = 0;
-      int rightY = 0;
-
-      for(int i = 0; i < 10; i++){ //sum 10 readings
-        leftX += analogRead(leftJoyX);
-        leftY += analogRead(leftJoyY);
-        rightX += analogRead(rightJoyX);
-        rightY += analogRead(rightJoyY);
-      }
-      // get average of 10 readings and map them to Joystick range
-      leftX /= 10;
-      leftY /= 10;
-      rightX /= 10;
-      rightY /= 10;
-
-      leftX = map(leftX, 0, 4095, 0, 32767);
-      leftY = map(leftY, 0, 4095, 0, 32767);
-      rightX = map(rightX, 0, 4095, 0, 32767);
-      rightY = map(rightY, 0, 4095, 0, 32767);       
-
-      bleGamepad.setLeftThumb(leftX,leftY);
-      bleGamepad.setRightThumb(rightX,rightY);
-
-      // Serial.print("rightX: ");
-      // Serial.print(rightX);
-      // Serial.print(", rightY: ");
-      // Serial.println(rightY);
     }
+
+    //Set Joystick Values. Note: Joysticks need 3.3V?
+
+    int leftX = 0;
+    int leftY = 0;
+    int rightX = 0;
+    int rightY = 0;
+
+    for(int i = 0; i < 10; i++){ //sum 10 readings
+      leftX += analogRead(leftJoyX);
+      leftY += analogRead(leftJoyY);
+      rightX += analogRead(rightJoyX);
+      rightY += analogRead(rightJoyY);
+    }
+    // get average of 10 readings and map them to Joystick range
+    leftX /= 10;
+    leftY /= 10;
+    rightX /= 10;
+    rightY /= 10;
+
+    leftX = map(leftX, 0, 4095, 32767, 0);
+    leftY = map(leftY, 0, 4095, 0, 32767);
+    rightX = map(rightX, 0, 4095, 32767, 0);
+    rightY = map(rightY, 0, 4095, 0, 32767);       
+
+    bleGamepad.setLeftThumb(leftX,leftY);
+    bleGamepad.setRightThumb(rightX,rightY);
+  }
 }
